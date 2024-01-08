07a98c5230edaa9328bc000ec88a3ba3483f07c5
José Fernando Mendes da Costa <98fernandocosta@gmail.com>
2024-01-08 12:48:50 -0300

    projeto criado com o initializr

---
+++ src/test/java/br/edu/ufape/usuarios/UsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ufape.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
