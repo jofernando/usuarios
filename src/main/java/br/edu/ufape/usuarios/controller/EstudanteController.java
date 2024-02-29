744285c97b44111574dd1c964c4eef428610a239
José Fernando Mendes da Costa <98fernandocosta@gmail.com>
2024-02-29 15:02:04 -0300

    adiciona anotacoes de pre autorizacao comentadas

--- src/main/java/br/edu/ufape/usuarios/controller/EstudanteController.java
+++ src/main/java/br/edu/ufape/usuarios/controller/EstudanteController.java
@@ -3,6 +3,9 @@ package br.edu.ufape.usuarios.controller;
 import br.edu.ufape.usuarios.controller.dto.EstudanteResponseDTO;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 import br.edu.ufape.usuarios.controller.dto.EstudanteRequestDTO;
@@ -27,21 +30,25 @@ public class EstudanteController {
 	}
 
 	@GetMapping
+//	@PreAuthorize("hasRole('GESTOR')")
 	public List<EstudanteResponseDTO> buscarTodos() {
 		return usuarios.recuperarEstudantes().stream().map(EstudanteResponseDTO::new).toList();
 	}
 
 	@GetMapping("{id}")
+//	@PreAuthorize("hasAnyRole('ESTUDANTE', 'GESTOR', 'PROFESSOR')")
 	public EstudanteResponseDTO buscarPorId(@PathVariable Long id) {
 		return new EstudanteResponseDTO(usuarios.recuperarEstudante(id));
 	}
 
 	@PatchMapping("{id}")
+//	@PreAuthorize("hasAnyRole('ESTUDANTE', 'GESTOR', 'PROFESSOR')")
 	public EstudanteResponseDTO alterar(@PathVariable Long id, @RequestBody @Valid EstudanteRequestDTO requestDTO) {
 		return new EstudanteResponseDTO(usuarios.atualizar(id, requestDTO.convertToEntity()));
 	}
 
 	@DeleteMapping("{id}")
+//	@PreAuthorize("hasAnyRole('GESTOR', 'PROFESSOR')")
 	public void deletar(@PathVariable Long id) {
 		usuarios.deletarEstudante(id);
 	}
