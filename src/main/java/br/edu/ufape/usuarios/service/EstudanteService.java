72d8062d428a6c47ac33783e7b8f6d0145867e33
José Fernando Mendes da Costa <98fernandocosta@gmail.com>
2024-02-29 10:31:47 -0300

    remove modificadores de acesso

--- src/main/java/br/edu/ufape/usuarios/service/EstudanteService.java
+++ src/main/java/br/edu/ufape/usuarios/service/EstudanteService.java
@@ -5,9 +5,9 @@ import java.util.List;
 import br.edu.ufape.usuarios.model.Estudante;
 
 public interface EstudanteService {
-	public List<Estudante> recuperarTodos();
-	public Estudante recuperar(Long id);
-	public Estudante cadastrar(Estudante estudante);
-	public Estudante atualizar(Long id, Estudante estudanteRequest);
-	public void deletar(Long id);
+	List<Estudante> recuperarTodos();
+	Estudante recuperar(Long id);
+	Estudante cadastrar(Estudante estudante);
+	Estudante atualizar(Long id, Estudante estudante);
+	void deletar(Long id);
 }
